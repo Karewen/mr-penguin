79bd1ad4aa89be5c9162613b7816879171cae73e
Karewen <60471956+Karewen@users.noreply.github.com>
2020-07-02 17:41:13 +0600

    Add files via upload

---
+++ commands/forecast.js
@@ -0,0 +1,41 @@
+const Discord = require(`discord.js`)
+
+const fetch = require('node-fetch');
+
+module.exports = {
+	name: 'forecast',
+  cooldown: 0.5,
+	description: 'Loot status.',
+	execute(message, args) {
+
+     let client = message.client;
+
+     let owner = client.users.cache.get('712329796931158016');
+    
+     fetch('http://clashofclansforecaster.com/STATS.json')
+     .then(result => result.json())
+     .then(async json => {
+       
+       let forecastWordNow = json.forecastWordNow
+       
+       let forecastMessage = json.forecastMessages['english']
+       
+       let embed = new Discord.MessageEmbed()
+       .setTitle(`Loot Forecaster`)
+       .setColor("RANDOM")
+       .addField(`Loot availability:`, forecastWordNow)
+       .addField(`Farmer's Forcast:`, forecastMessage)
+       .addField(`Credits to:`, "[Clash of Clans Forecaster](http://clashofclansforecaster.com/)")
+       .setFooter(`Created by: ${owner.tag}`, owner.displayAvatarURL())
+       
+       message.channel.send(embed)
+       
+     })
+     .then(err => {
+       if(err){
+         return console.log(err)
+       }
+     });
+    
+  },
+}
---
+++ index.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+
+const Discord = require('discord.js');
+
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+client.login(process.env.token);
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+client.on('ready', () => {
+    console.log(`I am ready and online as ${client.user.tag}`)
+    client.user.setActivity("loot statistics.", {type: "WATCHING"})
+})
+
+client.on('message', message => {
+
+    let prefix = "~"
+  
+      if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+      const args = message.content.slice(prefix.length).split(/ +/);
+      const commandName = args.shift().toLowerCase();
+  
+      const command = client.commands.get(commandName)
+          || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  
+      if (!command) return;
+  
+      if (!cooldowns.has(command.name)) {
+          cooldowns.set(command.name, new Discord.Collection());
+      }
+  
+      const now = Date.now();
+      const timestamps = cooldowns.get(command.name);
+      const cooldownAmount = (command.cooldown) * 60 * 1000;
+  
+      if (timestamps.has(message.author.id)) {
+          const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+  
+          if (now < expirationTime) {
+              const timeLeft = (expirationTime - now) / (60 * 1000);
+              return message.reply(`please wait ${(timeLeft.toFixed(1))} minute(s) more before reusing the \`~${command.name}\` command.`);
+          }
+      }
+  
+      timestamps.set(message.author.id, now);
+      setTimeout(() => timestamps.delete(message.author.id), cooldownAmount); 
+  
+      try {
+          command.execute(message, args);
+      } catch (error) {
+          console.error(error);
+          message.reply('there was an error trying to execute that command!');
+      }
+  });  
+
+
