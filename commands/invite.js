67376664d1e9c1bc2c4bbd72231ce3eb156e15e8
Karewen <60471956+Karewen@users.noreply.github.com>
2020-07-05 16:53:35 +0600

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,15 +4,16 @@ const { stripIndents } = require("common-tags");
 
 module.exports = {
 	name: 'invite',
-  cooldown: 0.5,
+  	cooldown: 0.5,
 	description: 'Invite the bot to your server.',
 	execute(message, args) {
 
-     let client = message.client;
+     	let client = message.client;
        
-       let embed = new Discord.MessageEmbed()
-       .addField("Invite", `[Invite the bot to your server!](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`);
-       message.channel.send(embed)
+        let embed = new Discord.MessageEmbed()
+	.setColor("#00ae86")
+        .addField("Mr. P Bot Invite", `[Invite the bot to your server!](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`);
+        message.channel.send(embed)
     
   },
 }
